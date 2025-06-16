f684a23a9b94fc3e37e111b4ce0733f281c3ce30
Alexandr Bulancov <a.bulancov@dev.igooods.ru>
2025-06-16 16:30:48 +0300

    Add Warden and RailsWarden, try to authenticate user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,24 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  # Add helper methods for authentication.
+  include RailsWarden::Authentication
+
+  # Temporarily use any user to authenticate.
+  # TODO: Replace with actual sessions.
+  before_action :temp_set_user
+
+  before_action :check_authorization
+
+  private
+
+  def temp_set_user
+    admin = User.first or raise "Create admin user to run this app"
+    login!(admin)
+  end
+
+  def check_authorization
+    authenticated? || head(:unauthorized)
+  end
 end
---
+++ config/initializers/warden.rb
@@ -0,0 +1,34 @@
+Warden::Strategies.add(:password) do
+  def valid?
+    params["login"] || params["password"]
+  end
+
+  def authenticate!
+    access_granted =
+      if (user = User.find_by(email: params["login"]))
+        user.authenticate(params["password"])
+      else
+        # Compare to a completely random password to waste time and prevent timing-based attacks.
+        BCrypt::Password.new("$2a$12$r.d.0EcomoBScMpE75pFGeAcd0MM2wYiFRRm7W4KTP0E7NbP61T1a") ==
+          params["password"]
+        false
+      end
+
+    if access_granted
+      success!(user)
+    else
+      fail!
+    end
+  end
+end
+
+Rails.configuration.middleware.use RailsWarden::Manager do |manager|
+  manager.failure_app = Proc.new { |_env|
+    [ "401", { "Content-Type" => "text/plain" }, "🖕" ]
+  }
+  manager.default_strategies :password # needs to be defined
+  manager.default_scope = :user # optional default scope
+  # Optional Settings (see Warden wiki)
+  # manager.scope_defaults :admin, strategies: [:password]
+  # manager.intercept_401 = false # Warden will intercept 401 responses, which can cause conflicts
+end
