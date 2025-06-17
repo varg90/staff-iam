a9d5ad517d91685f8a42fd307e7076e919416e90
Alexandr Bulancov <a.bulancov@dev.igooods.ru>
2025-06-17 16:05:28 +0300

    Add exclusions for rules, fix Layout/LineLength

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  # Only allow modern browsers supporting
+  # webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
   # Add helper methods for authentication.
